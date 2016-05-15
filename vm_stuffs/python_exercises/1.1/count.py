4b9e168f8b6524c03ec80cbaabe91e83af8c358a
Sean O'Connor <sean@bit.ly>
2016-05-14 21:32:18 -0400

    Added second python exercise.

--- vm_stuffs/python_exercises/1.1/count.py
+++ vm_stuffs/python_exercises/1.1/count.py
@@ -6,17 +6,17 @@ import ujson as json
 
 
 if __name__ == '__main__':
-	# Initialize the counter
-	clicks = Counter()
+    # Initialize the counter
+    clicks = Counter()
 
-	# Read from STDIN one line at a time
-	for line in sys.stdin:
-		# Parse the line as JSON
-		data = json.loads(line)
+    # Read from STDIN one line at a time
+    for line in sys.stdin:
+        # Parse the line as JSON
+        data = json.loads(line)
 
-		# Increment the counter for the global hash
-		clicks[data['g']] += 1
+        # Increment the counter for the global hash
+        clicks[data['g']] += 1
 
-	# Output our results
-	for ghash, count in clicks.iteritems():
-		print('{}\t{}'.format(ghash, count))
+    # Output our results
+    for ghash, count in clicks.iteritems():
+        print('{}\t{}'.format(ghash, count))
---
+++ vm_stuffs/python_exercises/1.2/count.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from collections import Counter
+import sys
+
+import ujson as json
+
+
+if __name__ == '__main__':
+    # Initialize the counters
+    link_clicks = Counter()
+    country_clicks = Counter()
+
+    # Read from STDIN one line at a time
+    for line in sys.stdin:
+        # Parse the line as JSON
+        data = json.loads(line)
+
+        # Increment the counter for the global hash
+        link_clicks[data['g']] += 1
+
+        # Increment the counter for the country
+        country_clicks[data['c']] += 1
+
+    # Output our results
+    for ghash, count in link_clicks.iteritems():
+        print('link\t{}\t{}'.format(ghash, count))
+
+    for ghash, count in country_clicks.iteritems():
+        print('country\t{}\t{}'.format(ghash, count))
